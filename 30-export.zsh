e0712ea0833ea310d3d2ea57368451d22fa8c681
David Arnold <80532381+optimisedu@users.noreply.github.com>
2025-02-14 19:49:18 +0000

    completion setup options to be updated
    credit to VALODIM

---
+++ 30-export.zsh
@@ -0,0 +1,17 @@
+
+# coloring in less, for manpages
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
+export PAGER=less
+
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
+
+LOGCHECK=60
+WATCHFMT="[%B%t%b] %B%n%b has %a %B%l%b from %B%M%b"
---
+++ 30-misc.zsh
@@ -0,0 +1,43 @@
+# cd to home and one path up directly
+# cdpath=(~ .. ~/space)
+
+## search for various types or README file in dir and display them in $PAGER
+readme ()
+{
+    local files
+    files=(./(#i)*(read*me|lue*m(in|)ut)*(ND))
+    if (($#files))
+    then $PAGER $files
+    else
+    print 'No README files.'
+    fi
+}
+
+# If nonnegative, commands whose combined user and system execution times
+# (measured in seconds) are greater than this value have timing
+# statistics printed for them.
+REPORTTIME=2
+# This is the default TIMEFORMAT string, but with a bell. Ding ding. :)
+TIMEFORMAT="%J  %U user %S system %P cpu %*E total"$'\a'
+
+function cs() { cd "$1" && ls }
+function mkcd() { mkdir "$1" && cd "$1"; }
+
+function hex2dec { awk 'BEGIN { printf "%d\n",0x$1}'; }
+function dec2hex { awk 'BEGIN { printf "%x\n",$1}'; }
+
+autoload -U zargs
+autoload -U zmv
+
+# add less -F capabilities to grep
+# hm. doesn't work as expected...
+# grep() {
+    # command grep "$@" | less -F
+# }
+
+autoload -U :h
+
+# load datetime, stat and mathfunc modules
+# zmodload zsh/datetime
+# zmodload zsh/stat
+# zmodload zsh/mathfunc
